dfa78f7cb90cfdfb2887d43e27e3454a04faf8ae
Dirbaio <dirbaio@dirbaio.net>
2012-10-22 17:16:08 +0200

    Improved level entrances.

--- source/entrances.s
+++ source/entrances.s
@@ -1,4 +1,17 @@
 
+@=================================
+@ Macro for printing stuff from ASM code
+
+.macro print txt
+mov r12,r12
+b 1f
+.word 0x6464
+.ascii "\txt"
+.byte 0
+.align 4
+1:
+.endm
+
 @ Replace entrance func.
 nsub_0201E744:
 	PUSH	{LR}
@@ -11,9 +24,11 @@ nsub_0201E744:
 	LDRNE 	R12, =0x0201E74C
 	BXNE	R12
 	
-	MOV		R0, #0
 	LDR		R1, =entranceDestEntrance
 	LDR		R1, [R1]
+	MOV		R0, R1, LSR#4
+	AND		R1, R1, #0xF
+	print 	"Load %r0% %r1%"
 	BL		_Z11myLoadLevelii
 	
 	ADD		SP, SP, #0xC
